55cd3498b01c76db9201c6c144b002c9d2412a56
Mateus Clever da Silva Miranda <mateus.clever@gmail.com>
2024-12-31 16:51:14 -0300

    novos conhecimentos

---
+++ aula 11/ex008.js
@@ -0,0 +1,7 @@
+console.log('o console funcionou corretamente')
+var vel = 70
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+if (vel >= 60) {
+    console.log(`Você ultrapassou a velocidade permitida: MULTADO!`)
+}
+console.log(`Dirija sempre com o cinto  de segurança`)
