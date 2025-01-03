182e188ad47bbefad69dc53267c915d245175235
Mateus Clever da Silva Miranda <mateus.clever@gmail.com>
2025-01-03 17:06:43 -0300

    execico hora certa

--- ex013/script.js
+++ ex013/script.js
@@ -1,1 +1,17 @@
-alert('Olá!')
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('img')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12 ) {
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#525154'
+    }
+}
